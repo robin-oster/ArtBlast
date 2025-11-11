e4b5c1d3ce31de57e75103ea1fa31ee79d3ca3eb
moster <mostost96@gmail.com>
2025-11-11 16:03:22 -0500

    Initial Commit

---
+++ src/main/java/dev/ArtBlast/ArtBlastApplication.java
@@ -0,0 +1,13 @@
+package dev.ArtBlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtBlastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtBlastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ArtBlast/ArtBlastApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ArtBlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtBlastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
